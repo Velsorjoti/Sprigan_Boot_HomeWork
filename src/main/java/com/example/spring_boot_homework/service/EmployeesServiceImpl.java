1974310d092940c917ad15e93152bd33d5fff52f
Velsorjoti <odzennedvijiimii@gmail.com>
2023-03-02 14:56:19 +0400

    Домашняя работа по весне загрузочной, задания №2.

--- src/main/java/com/example/spring_boot_homework/service/EmployeesServiceImpl.java
+++ src/main/java/com/example/spring_boot_homework/service/EmployeesServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.spring_boot_homework.service;
 
-import com.example.spring_boot_homework.dao.EmployeesDAO;
+import com.example.spring_boot_homework.dao.EmployeesRepo;
 import com.example.spring_boot_homework.entity.Employees;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,41 +9,34 @@ import java.util.List;
 
 @Service
 public class EmployeesServiceImpl implements EmployeesService {
-    private EmployeesDAO employeesDAO;
+    private EmployeesRepo employeesRepo;
 
-    public EmployeesServiceImpl(EmployeesDAO employeesDAO) {
-        this.employeesDAO = employeesDAO;
+    public EmployeesServiceImpl(EmployeesRepo employeesRepo) {
+        this.employeesRepo = employeesRepo;
     }
 
     @Override
-    @Transactional
     public List<Employees> getEmployees() {
-        return employeesDAO.getEmployees();
+        return employeesRepo.findAll();
     }
 
     @Override
-    @Transactional
     public Employees getEmployeesById(int id) {
-        Employees employees = employeesDAO.getEmployeesById(id);
-        return employees;
+        return employeesRepo.findById(id).get();
     }
 
     @Override
-    @Transactional
     public void addEmployees(Employees employees) {
-        employeesDAO.addEmployees(employees);
+        employeesRepo.save(employees);
     }
 
     @Override
-    @Transactional
     public void updateEmployees(Employees employees) {
-        employeesDAO.updateEmployees(employees);
+        employeesRepo.save(employees);
     }
 
     @Override
-    @Transactional
     public void deleteEmployees(int id) {
-        Employees employees = employeesDAO.getEmployeesById(id);
-        employeesDAO.deleteEmployees(id);
+        employeesRepo.deleteById(id);
     }
 }
